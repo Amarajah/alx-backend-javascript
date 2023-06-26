d1e82324ac9e149b4ead1765435228cfc9d255da
Amarajah <naomi.ibeh69@gmail.com>
2023-06-26 22:15:53 +0000

    For loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (idx in array) {
+    value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
