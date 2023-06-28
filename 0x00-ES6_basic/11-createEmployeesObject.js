cae392b2a20bad7e8ee60ce49f7720a34fd88788
Naomi Ibe <naomi.ibeh69@gmail.com>
2023-06-28 12:24:49 +0100

    Update 11-createEmployeesObject.js

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,8 @@
 export default function createEmployeesObject(departmentName, employees) {
-  return {
+  const employeesObject = {
     [departmentName]: employees,
   };
+
+  return employeesObject;
 }
 
