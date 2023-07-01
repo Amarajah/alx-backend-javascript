cf829f947677fe78301eec60267dab890721bbff
Amarajah <naomi.ibeh69@gmail.com>
2023-07-01 21:42:20 +0000

    Promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,16 @@
 export default function getResponseFromAPI() {
-  return new Promise(() => { console.log(true); });
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call
+    setTimeout(() => {
+      const response = 'API response';
+      const isError = false;
+
+      if (!isError) {
+        resolve(response); // Resolve the promise with the response
+      } else {
+        reject(new Error('API error')); // Reject the promise with an error
+      }
+    }, 2000); // Simulating a 2-second delay
+  });
 }
+
