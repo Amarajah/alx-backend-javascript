617d06bf82b463094838bcd509e3101fbebe6960
Naomi Ibe <naomi.ibeh69@gmail.com>
2023-07-03 23:10:00 +0100

    Update 2-get_students_by_loc.js

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,5 +2,5 @@ export default function getStudentsByLocation(listOfStudents) {
   if (!Array.isArray(listOfStudents)) {
     return [];
   }
-    return listOfStudents.filter(student => student.location);
+    return listOfStudents.filter((student) => student.location);
 }
