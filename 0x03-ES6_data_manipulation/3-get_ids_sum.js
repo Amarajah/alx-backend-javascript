184a13b906ba17f5ccd1b45aa8f7b60bee060a88
Naomi Ibe <naomi.ibeh69@gmail.com>
2023-07-03 23:16:58 +0100

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,7 +2,7 @@ export default function getStudentIdsSum(listOfStudents) {
   if (!Array.isArray(listOfStudents)) {
     return [];
   }
-    return listOfStudents.reduce((sum, student) => {
-      return sum + student.id
-    }, 0);
+  return listOfStudents.reduce((sum, student) => {
+    return sum + student.id;
+  }, 0);
 }
